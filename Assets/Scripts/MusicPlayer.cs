f23ea3860e0bc6220364c40290ff999cb3028f1a
Akash Jaiswar <akash.jaiswar41@gmail.com>
2019-08-06 11:51:08 +0530

    Added music for every different levels
    New music loads on eb=vry different levels

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class MusicPlayer : MonoBehaviour
 {
     static MusicPlayer instance = null;
-    // void Start(){
-    //     Debug.Log("Music player start" + GetInstanceID());       
-    // }
-    void Awake(){
+
+    public AudioClip startClip;
+    public AudioClip gameClip;
+    public AudioClip endClip;
+
+    private AudioSource music;
+
+    void Start(){
         //Debug.Log("Music player awake" + GetInstanceID()); 
-        if(instance != null){
+        if(instance != null && instance != this){
             Destroy(gameObject);
             Debug.Log("Duplicate Destroyed");
         }else{
             instance = this;
             GameObject.DontDestroyOnLoad(gameObject);
+            music = GetComponent<AudioSource>();
+            music.clip = startClip;
+            music.loop = true;
+            music.Play();
         }
+    }
+
+    void OnLevelWasLoaded(int level){
+        Debug.Log("Music Player Loaded Level : " + level);
+        music.Stop();
+        if(level == 0){
+            music.clip = startClip;
+        }else if(level == 1){
+            music.clip = gameClip;
+        }else if(level == 2){
+            music.clip = endClip;
+        }
+        music.loop = true;
+        music.Play();
     }
 }
